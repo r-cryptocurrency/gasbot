dca664998561dfd390986d28c430bd255be9c54b
Mmackz <matthewmackenz@gmail.com>
2023-08-09 16:49:06 -0700

    add function to check distribution csvs

--- gasbot/distribution_csv.py
+++ gasbot/distribution_csv.py
@@ -1,7 +1,9 @@
 import os
+import csv
 import requests
 from datetime import datetime, timedelta
 from gasbot.constants import START_SNAPSHOT_DATE, START_SNAPSHOT_ROUND
+
 
 def calculate_snapshot_date():
     now = datetime.utcnow()
@@ -9,6 +11,7 @@ def calculate_snapshot_date():
     while now >= snapshot_date:  
         snapshot_date += timedelta(days=28)
     return snapshot_date
+
 
 def calculate_round_number():
     """
@@ -47,3 +50,27 @@ def download_and_save_csv():
         print(f"Failed to download CSV for round {round_number - 1}")
 
     print(f"*******Next update scheduled for {calculate_snapshot_date()} UTC*******")
+
+
+def check_address_in_csvs(vault_address):
+    """
+    Checks if the address is present in the distribution CSVs with at least 5 karma.
+    
+    Args:
+        address (str): The address to verify.
+        
+    Returns:
+        tuple: (karma_in_current_csv, karma_in_previous_csv)
+    """
+    def check_address(file_path):
+        with open(file_path, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                if row["blockchain_address"].lower() == vault_address.lower() and int(row["karma"]) >= 5:
+                    return int(row["karma"])
+        return 0
+    
+    karma_in_current_csv = check_address("data/distribution_current.csv")
+    karma_in_previous_csv = check_address("data/distribution_previous.csv")
+    
+    return karma_in_current_csv, karma_in_previous_csv
